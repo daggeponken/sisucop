0f747bc86c18fe37397c7c5e07bad91329ee325a
daggeponken <dag.ehlin@gmail.com>
2020-12-01 16:12:27 +0100

    Update secrets.h

--- secrets.h
+++ secrets.h
@@ -2,7 +2,7 @@
 // and connection details
 
 #define SECRET_SSID "ComHemF9A028"		// replace MySSID with your WiFi network name
-#define SECRET_PASS "3vza9jmm"	// replace MyPassword with your WiFi password
+#define SECRET_PASS ""	// replace MyPassword with your WiFi password
 
 #define SECRET_CH_ID 1245727			// replace 0000000 with your channel number
 #define SECRET_WRITE_APIKEY "NLII4O9I9C2D78QJ"   // replace XYZ with your channel write API Key
